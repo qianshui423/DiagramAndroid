948bbd2573dfb72889f5514f64739a00e3ea013d
liuxuehao <liuxuehao@souche.com>
2021-02-06 05:42:21 +0800

    feat: append SyncBarrier

--- app/src/main/java/com/diagram/diagramandroid/handler/SyncBarrierTestActivity.java
+++ app/src/main/java/com/diagram/diagramandroid/handler/SyncBarrierTestActivity.java
@@ -6,9 +6,9 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.MessageQueue;
+import android.os.SystemClock;
 import android.view.View;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.diagram.diagramandroid.R;
@@ -16,8 +16,11 @@ import com.diagram.diagramandroid.util.LogUtils;
 
 import java.lang.reflect.Method;
 import java.util.ArrayDeque;
-import java.util.Queue;
 
+/**
+ * 同步屏障技术 兼容4.1及以上
+ * 4.1以后Android重构了屏幕刷新机制，新增 Choreographer
+ */
 public class SyncBarrierTestActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "SyncBarrierTestActivity";
@@ -64,15 +67,10 @@ public class SyncBarrierTestActivity extends AppCompatActivity implements View.O
     }
 
     //往消息队列插入同步屏障
-    @RequiresApi(api = Build.VERSION_CODES.M)
     public void sendSyncBarrier() {
         try {
             LogUtils.d("插入同步屏障");
-            // getQueue api 需要系统版本为6.0及以上
-            MessageQueue queue = mHandler.getLooper().getQueue();
-            Method method = MessageQueue.class.getDeclaredMethod("postSyncBarrier");
-            method.setAccessible(true);
-            int token = (int) method.invoke(queue);
+            int token = addSyncBarrier(SystemClock.uptimeMillis());
             mSyncBarrierTokens.offer(token);
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,30 +78,55 @@ public class SyncBarrierTestActivity extends AppCompatActivity implements View.O
     }
 
     //往消息队列插入同步屏障
-    @RequiresApi(api = Build.VERSION_CODES.M)
     public void sendSyncBarrier(long when) {
         try {
             LogUtils.d("插入同步屏障");
-            // getQueue api 需要系统版本为6.0及以上
-            MessageQueue queue = mHandler.getLooper().getQueue();
-            Method method = MessageQueue.class.getDeclaredMethod("postSyncBarrier");
-            method.setAccessible(true);
-            int token = (int) method.invoke(queue, when);
+            int token = addSyncBarrier(when);
             mSyncBarrierTokens.offer(token);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private int addSyncBarrier(long when) {
+        try {
+            MessageQueue queue;
+            Method method;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                queue = mHandler.getLooper().getQueue();
+                method = MessageQueue.class.getDeclaredMethod("postSyncBarrier");
+            } else {
+                queue = Looper.myQueue();
+                // 6.0 以前需要使用 enqueueSyncBarrier 添加屏障消息
+                method = MessageQueue.class.getDeclaredMethod("enqueueSyncBarrier");
+            }
+            method.setAccessible(true);
+            return (int) method.invoke(queue, when);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
     //移除屏障
-    @RequiresApi(api = Build.VERSION_CODES.M)
     public void removeSyncBarrier() {
         Integer token = mSyncBarrierTokens.poll();
         if (token == null) return;
+        LogUtils.d("移除同步屏障");
+        removeSyncBarrier(token);
+    }
+
+    private void removeSyncBarrier(int token) {
         try {
             LogUtils.d("移除同步屏障");
-            // getQueue api 需要系统版本为6.0及以上
-            MessageQueue queue = mHandler.getLooper().getQueue();
+            MessageQueue queue = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                // 6.0 以后新增
+                queue = mHandler.getLooper().getQueue();
+            } else {
+                // 6.0 以前需要使用下面的方法获取消息队列
+                queue = Looper.myQueue();
+            }
             Method method = MessageQueue.class.getDeclaredMethod("removeSyncBarrier", int.class);
             method.setAccessible(true);
             method.invoke(queue, token);
@@ -121,17 +144,24 @@ public class SyncBarrierTestActivity extends AppCompatActivity implements View.O
     }
 
     //往消息队列插入异步消息
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
     private void sendAsyncMessage() {
         LogUtils.d("插入异步消息");
         Message message = Message.obtain();
         message.what = MESSAGE_TYPE_ASYNC;
-        // setAsynchronous api 需要系统版本为5.1及以上
-        message.setAsynchronous(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            // 5.1 开放了 setAsynchronous 为公有方法
+            message.setAsynchronous(true);
+        } else {
+            try {
+                Method method = Message.class.getDeclaredMethod("setAsynchronous", boolean.class);
+                method.invoke(message, true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         mHandler.sendMessageDelayed(message, 1000);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onClick(View v) {
         int id = v.getId();
